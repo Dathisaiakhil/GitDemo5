7657cf77ef182b5100354f9aa73420398ad47141
SaiAkhil <dathi30081997@gmail.com>
2021-11-11 08:37:59 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿amusing System;
+
+namespace GitDemo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
